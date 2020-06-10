9d9ea004a00602f9203ec08097f2a20e983c913a
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-10 21:44:57 +0900

    moved the txt file to another folder and imported by the deno way

--- mod.ts
+++ mod.ts
@@ -1,8 +1,11 @@
+import { join } from "https://deno.land/std/path/mod.ts";
+
 async function readFile() {
-  const data = await Deno.readTextFile("hello.txt");
-  for await (const dirEntry of Deno.readDir(".")) {
-    console.log(dirEntry.name);
-  }
+  const path = join("text_file", "hello.txt")
+  const data = await Deno.readTextFile(path);
+  // for await (const dirEntry of Deno.readDir(".")) {
+  //   console.log(dirEntry.name);
+  // }
   console.log(data)
 }
 
